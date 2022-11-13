289950a12ddf591cb82a7f7d2ade24fc1c7139bd
AGFMT <albingeorge2016@gmail.com>
2022-11-13 12:08:41 +0530

    one

---
+++ array addition.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+    int array[100];
+    int limit,i;
+    int sum=0;
+    printf("\nENTER THE LIMIT OF THE ARRAY");
+    scanf("%d",&limit);
+    printf("\nEnter the values of the array");
+    for ( i = 0; i < limit; i++)
+    {
+        scanf("%d",array[i]);
+    }
+    for ( i = 0; i < limit; i++)
+    {
+         sum=sum+array[i];
+    }
+        printf("The sum is=%d",sum);
+        
+    
+
+    return EXIT_SUCCESS;
+
+}
